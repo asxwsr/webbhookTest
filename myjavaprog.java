d5aad323820a17c93d36fc9acb974324f5641c2b
asx <asxwsr@gamil.com>
2025-09-05 01:50:22 +1000

    new

--- myjavaprog.java
+++ myjavaprog.java
@@ -1,7 +1,7 @@
 // HelloWorld.java
 public class myjavaprog {
     public static void main(String[] args) {
-        System.out.println("Hello, hello  World!");
+        System.out.println("Hello there, hello  World!");
     }
 }
 
